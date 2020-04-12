e82803fde859156e0336d15023e381c27ba522d9
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-12 14:29:07 +1000

    Custom Auth Middleware & JWT Verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -64,7 +64,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtToken'),
+        config.get('jwtSecret'),
         { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
