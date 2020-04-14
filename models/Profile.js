389c135de941973bcfc10cc1eace6a09a895ed78
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-14 14:53:05 +1000

    Creating The Profile Model

--- models/Profile.js
+++ models/Profile.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+const ProfileSchema = new.mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+});
