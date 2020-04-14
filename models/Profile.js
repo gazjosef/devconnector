ec036c6738c2d73b7d0e7f48137704fc5c6676b0
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-14 20:00:06 +1000

    Get Current User Profile

--- models/Profile.js
+++ models/Profile.js
@@ -57,4 +57,57 @@ const ProfileSchema = new.mongoose.Schema({
       },
     },
   ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        required: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
